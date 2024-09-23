3ebcd107a68bb62982389f72b9669b3346c25949
Christian Foidl <christian@wunderwerk.io>
2024-09-23 10:19:44 +0200

    build: refactor to monorepo

---
+++ packages/config/eslint/base.mjs
@@ -0,0 +1,138 @@
+import eslint from "@eslint/js";
+import prettiereslint from "eslint-config-prettier";
+import jsdoceslint from "eslint-plugin-jsdoc";
+import tseslint from "typescript-eslint";
+
+/**
+ * The base eslint config for typescript.
+ */
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  jsdoceslint.configs["flat/recommended-typescript"],
+  prettiereslint,
+  {
+    rules: {
+      "no-var": "warn",
+      "no-empty": "warn",
+      "no-unused-vars": [
+        "warn",
+        {
+          varsIgnorePattern: "^_",
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "no-console": [
+        "warn",
+        {
+          allow: ["assert"],
+        },
+      ],
+      "no-fallthrough": "warn",
+      "no-case-declarations": "error",
+      "no-self-assign": "warn",
+      "new-cap": "off",
+      "no-undefined": "off",
+      "no-global-assign": "error",
+      "prefer-const": "error",
+      "dot-notation": "error",
+      "no-useless-catch": "error",
+      "no-prototype-builtins": "off",
+      "jsdoc/require-description-complete-sentence": [
+        "warn",
+        { abbreviations: ["etc", "e.g.", "i.e."] },
+      ],
+      "jsdoc/require-hyphen-before-param-description": "warn",
+      "jsdoc/no-blank-block-descriptions": "error",
+      "jsdoc/tag-lines": ["warn" | "warn", "never", { startLines: 1 }],
+      "jsdoc/require-jsdoc": [
+        "warn",
+        {
+          publicOnly: false,
+          exemptEmptyFunctions: false,
+          exemptEmptyConstructors: false,
+          require: {
+            ArrowFunctionExpression: false,
+            ClassDeclaration: false,
+            ClassExpression: false,
+            FunctionDeclaration: false,
+            FunctionExpression: false,
+            MethodDefinition: false,
+          },
+          contexts: [
+            "ClassDeclaration",
+            "ClassExpression",
+            // Select a FunctionDeclaration that is not a descendant of
+            // another FunctionDeclaration.
+            ":function:not(:function :function):not(CallExpression > :function):not(Property > :function)",
+          ],
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          varsIgnorePattern: "^_",
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/prefer-optional-chain": "error",
+      "@typescript-eslint/consistent-type-assertions": [
+        "error",
+        {
+          assertionStyle: "as",
+          objectLiteralTypeAssertions: "never",
+        },
+      ],
+      "jsdoc/require-returns": "off",
+      "jsdoc/require-param": "off",
+      "jsdoc/require-description-complete-sentence": [
+        "warn",
+        { abbreviations: ["etc", "e.g.", "i.e."] },
+      ],
+      "jsdoc/require-hyphen-before-param-description": "warn",
+      "jsdoc/no-blank-block-descriptions": "error",
+      "jsdoc/require-jsdoc": [
+        "warn",
+        {
+          publicOnly: false,
+          exemptEmptyFunctions: false,
+          exemptEmptyConstructors: false,
+          require: {
+            ArrowFunctionExpression: false,
+            ClassDeclaration: false,
+            ClassExpression: false,
+            FunctionDeclaration: false,
+            FunctionExpression: false,
+            MethodDefinition: false,
+          },
+          contexts: [
+            "ClassDeclaration",
+            "ClassExpression",
+            // Select a FunctionDeclaration that is not a descendant of
+            // another FunctionDeclaration.
+            ":function:not(:function :function):not(CallExpression > :function):not(Property > :function)",
+          ],
+        },
+      ],
+    },
+  },
+  {
+    files: ["**/*.stories.tsx", "**/*.stories.ts"],
+    rules: {
+      "no-case-declarations": "off",
+      "no-console": "off",
+      "@typescript-eslint/ban-ts-comment": [
+        "error",
+        {
+          "ts-ignore": "allow-with-description",
+        },
+      ],
+    },
+  },
+);
